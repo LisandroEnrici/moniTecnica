4b29d60f32c04e544093c9ef42e89333034ecbf8
LisandroEnrici <lisandro.enrici@stellantis.com>
2022-04-15 15:28:13 -0300

    MaterialUI, baseTheme e imagenes agregadas

---
+++ src/assets/theme.js
@@ -0,0 +1,40 @@
+
+const PALETTE = {
+  primary: {
+    main: '#76A9EC',
+    light: '#F1F6FB',
+    dark: '#6AA1EA'
+},
+secondary: {
+    main: '#23C68C',
+    light: '#baf0d1',
+    dark: '#6ea485'
+  },
+  error: {
+    main: '#C53814'
+  },
+  warning: {
+    main: '#FFD710'
+  },
+  info: {
+    main: '#1495C5'
+  },
+  success: {
+    main: '#0E9B2F'
+  },
+  grayScale: {
+    g0: '#FFFFFF',
+    g100: '#FAFAFA',
+    g200: '#E8E8E8',
+    g300: '#D1D1D1',
+    g400: '#717171'
+  },
+  black: {
+    main: '#000000'
+  }
+}
+
+export const baseTheme = {
+  palette: PALETTE,
+
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import { baseTheme } from './assets/theme.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme(baseTheme)
+
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+
+  </ThemeProvider>
 );
